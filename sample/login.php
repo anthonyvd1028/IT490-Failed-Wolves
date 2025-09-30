e9747274445e3ace6a9152861ff64a4b75c7f7ae
Anthony DiTaranto <avd8@njit.edu>
2025-09-30 00:24:20 -0400

    Login.php logic to send login requests to the rabbit server

--- sample/login.php
+++ sample/login.php
@@ -1,21 +1,31 @@
 <?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 
 if (!isset($_POST))
 {
-	$msg = "NO POST MESSAGE SET, POLITELY FUCK OFF";
-	echo json_encode($msg);
-	exit(0);
+        $msg = "NO POST MESSAGE SET, POLITELY FUCK OFF";
+        echo json_encode($msg);
+        exit(0);
 }
 $request = $_POST;
 $response = "unsupported request type, politely FUCK OFF";
 switch ($request["type"])
 {
-	case "login":
-		$response = "login, yeah we can do that";
+case "login":
+	$rabbitRequest = array();
+	$rabbitRequest['type'] = "login";
+	$rabbitRequest['username'] = $request["uname"];
+	$rabbitRequest['password'] = $request["pword"];
+	$response = $client->send_request($rabbitRequest);
 	break;
 }
-echo json_encode($response);
+ 
+echo json_encode($response["message"]);
 exit(0);
 
 ?>
+
