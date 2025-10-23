eaa6cc94080e25367b154bb4a9153ef5d1fbe552
Anthony DiTaranto <avd8@njit.edu>
2025-10-23 21:27:20 +0000

    Added email notification for the watchlist alerts

--- evalWatchlist.php
+++ evalWatchlist.php
@@ -1,12 +1,14 @@
 #!/usr/bin/php
-<?php 
+<?php
+require_once('Email.inc');
+
 $mydb = new mysqli('127.0.0.1', 'admin', 'AdminPass123!', 'IT_490');
 if ($mydb->connect_errno != 0) {
 	echo $mydb->connect_error . PHP_EOL;
 	exit(0);
 }
 
-$query = "SELECT * FROM Watchlist LEFT JOIN Odds ON Odds.OddID = Watchlist.OddID LEFT JOIN users ON users.id = Watchlist.UserID;";
+$query = "SELECT * FROM Watchlist LEFT JOIN Odds ON Odds.OddID = Watchlist.OddID LEFT JOIN users ON users.id = Watchlist.UserID LEFT JOIN Events ON Events.EventID = Odds.EventID;";
 $results = $mydb->query($query);
 if ($mydb->errno != 0) {
         echo "Failed to execute query" . PHP_EOL;
@@ -21,12 +23,16 @@ if ($results->num_rows > 0)
 		$eventId = $row['EventID'];
 		$sportsbook = $row['Sportsbook'];
 		$type = $row['BetType'];
+		$email = $row['email'];
+		$home = $row['HomeTeam'];
+		$away = $row['AwayTeam'];
 
 		$query = "SELECT * FROM Odds WHERE EventID = $eventId AND Sportsbook = '$sportsbook' AND BetType = '$type' ORDER BY OddID DESC;";
 		$results2 = $mydb->query($query);
 		$results2 = $results2->fetch_assoc();
 
 		$newOdd = $results2['Odds'];
+		$newOddId = $results2['OddID'];
 
 		if ($odd[0] === "+")
                 {
@@ -44,11 +50,17 @@ if ($results->num_rows > 0)
 
 		if ($oldOddDec > $newOddDec)
 		{
-			echo "Odds Havent Gotten Better" . PHP_EOL;
+			$subject = "Odds in your watchlist have changed";
+			$message = "The odds for $away @ $home and $type have went down";
+			SendEmail($email, $subject, $message);
 		} elseif ($oldOddDec < $newOddDec) {
-			echo "Odds Gotten Better" . PHP_EOL;
+			$subject = "Odds in your watchlist have changed";
+			$message = "The odds for $away @ $home and $type have went up";
+			SendEmail($email, $subject, $message);
 		} else {
-			echo "Odds Havent Changed" . PHP_EOL;
+			$subject = "Watchlist Alert";
+			$message = "The odds for $away @ $home and $type have not moved";
+			SendEmail($email, $subject, $message);
 		}
 	}
 }
