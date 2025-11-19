5401c70fa1b1928f70bc41d019d37e9a49da5b15
Anthony DiTaranto <avd8@njit.edu>
2025-11-19 00:09:08 +0000

    Initial listener on QA and Prod VMs and a modification to the ini file

---
+++ deployementAgent.php
@@ -0,0 +1,35 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function pullChanges($req)
+{
+	var_dump($req);
+
+
+	return;
+}
+
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  if(!isset($request['type'])) {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type']) {
+    case "pull":
+        return pullChanges($request);
+	break;
+  }
+  return array('message'=>"No valid type found");
+}
+
+$server = new rabbitMQServer("deployement.ini","deployementQA");
+
+echo "testRabbitMQServer BEGIN".PHP_EOL;
+$server->process_requests('requestProcessor');
+echo "testRabbitMQServer END".PHP_EOL;
+exit();
+?>
