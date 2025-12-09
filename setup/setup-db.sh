a2e5091bf2a5abb6703896d24c49c156f1054496
Anthony DiTaranto <anthonyvd1028@gmail.com>
2025-12-09 01:52:08 +0000

    Modified the service file to use my directory. Modified the setup scrip to remove the old service file first. Created an ini file for the qa listener to reflect the new IP.

--- setup/setup-db.sh
+++ setup/setup-db.sh
@@ -2,12 +2,12 @@
 
 rootDir=$1
 
-sed -E -i "s|^ExecStart=/home/jjr59/versions/[0-9]+/testRabbitMQServer\.php|ExecStart=${rootDir}testRabbitMQServer.php|" ${rootDir}setup/testRabbitMQServer.service
-echo -n "Which "
-which cp
+sed -E -i "s|^ExecStart=/home/avd8/versions/[0-9]+/testRabbitMQServer\.php|ExecStart=${rootDir}testRabbitMQServer.php|" ${rootDir}setup/testRabbitMQServer.service
 
-echo "Copying"
-sudo cp ${rootDir}setup/testRabbitMQServer.service /etc/systemd/system/
-
-echo "Reloading Daemon"
+sudo systemctl stop deployementAgent.service
+sudo rm /etc/systemd/system/deployementAgent.service
 sudo systemctl daemon-reload
+
+sudo cp ${rootDir}setup/testRabbitMQServer.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start deployementAgent.service
