9b2e4ed78d821ed012b4a12e15f3e35f4d14f3ba
Anthony DiTaranto <avd8@njit.edu>
2025-12-09 21:58:22 +0000

    Modified the start script for the db listener

--- setup/setup-db.sh
+++ setup/setup-db.sh
@@ -4,10 +4,10 @@ rootDir=$1
 
 sed -E -i "s|^ExecStart=/home/avd8/versions/[0-9]+/testRabbitMQServer\.php|ExecStart=${rootDir}testRabbitMQServer.php|" ${rootDir}setup/testRabbitMQServer.service
 
-sudo systemctl stop deployementAgent.service
-sudo rm /etc/systemd/system/deployementAgent.service
+sudo systemctl stop testRabbitMQServer.service
+sudo rm /etc/systemd/system/testRabbitMQServer.service
 sudo systemctl daemon-reload
 
 sudo cp ${rootDir}setup/testRabbitMQServer.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo systemctl start deployementAgent.service
+sudo systemctl start testRabbitMQServer.service
